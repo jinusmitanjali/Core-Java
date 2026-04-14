c775ba383f0f111d90a6d63f6669fd64042ae8d6
Smitanjali Moharana <jinusmitanjali@gmail.com>
2026-04-14 15:08:32 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,9 @@
+public class StudentDetails{
+    public static void main(String args[]){
+	System.out.println("Name : Smitanjali");
+	System.out.println("Age : 23");
+	System.out.println("Degree : MCA");
+	System.out.println("YOP : 2025");
+
+    }
+}
